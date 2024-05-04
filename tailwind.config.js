92cea5483866d895bed303587e178ea66c3aea20
Miguel Salazar <yobebe@hotmail.com.ar>
2024-05-04 15:57:13 -0300

    Esto es un quilombogit add . , viva bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+      colors: {
+        'negro': '#000',
+        'blanco': '#fff',
+        'azul': '#00255'
+      },
+  },
+  plugins: [],
+}
+
